d32f7cfeac4a4e3b5efb15f79a38936ac2b16be6
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-28 15:12:29 -0400

    Added lines in start.java that gets file names from the command line arguments

--- src/start.java
+++ src/start.java
@@ -1,6 +1,11 @@
 public class start {
 
     public static void main(String[] args){
-
+        if(args.length < 2) {
+            System.out.println("You must provide exactly two command line arguments.");
+            System.exit(0);
+        }
+        String team_file_name = args[0];
+        String bracket_file_name = args[1];
     }
 }

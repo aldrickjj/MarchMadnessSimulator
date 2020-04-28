80c4ff3e6b3ee3d697535b210b0dbcf71dc2f403
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-28 19:13:02 -0400

    implemented BracketReaderFactory

--- src/data/brackets/BracketReaderFactory.java
+++ src/data/brackets/BracketReaderFactory.java
@@ -1,4 +1,16 @@
 package data.brackets;
 
 public class BracketReaderFactory {
+
+    public BracketReader getBracketReader(String filename) {
+        if(filename.toLowerCase().contains("csv")) {
+            return new BracketReaderCSV(filename);
+        }
+        else if(filename.toLowerCase().contains("json")) {
+            return new BracketReaderJSON(filename);
+        }
+        else {
+            return null;
+        }
+    }
 }

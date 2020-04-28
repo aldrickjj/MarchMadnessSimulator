ee1b290aceddafe7baa8d3cd0fb2148ff41ccc9f
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-28 17:52:33 -0400

    implemented the logger class

--- src/logging/Logger.java
+++ src/logging/Logger.java
@@ -1,4 +1,66 @@
 package logging;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
 public class Logger {
+    public static Logger instance;
+    private String file_name;
+    private BufferedWriter writer;
+    private Date date;
+    private int line_number;
+
+    private Logger() {
+        line_number = 0;
+        date = new Date();
+        file_name = "log.txt";
+        try {
+            writer = new BufferedWriter(new FileWriter(file_name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        instance = this;
+    }
+
+    public Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void error(String className, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(date.toString()).append(" ERROR ").append(className)
+                .append(":").append(line_number).append(" - ").append(message);
+        try {
+            writer.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void info(String className, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(date.toString()).append(" INFO ").append(className)
+                .append(":").append(line_number).append(" - ").append(message);
+        try {
+            writer.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void trace(String className, String message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(date.toString()).append(" TRACE ").append(className)
+                .append(":").append(line_number).append(" - ").append(message);
+        try {
+            writer.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

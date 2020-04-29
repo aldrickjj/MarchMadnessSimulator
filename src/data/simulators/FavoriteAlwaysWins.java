18b199a5cb409aca491506b71172c580ab444a19
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-28 21:24:01 -0400

    implemented FavoriteAlwaysWins

--- src/data/simulators/FavoriteAlwaysWins.java
+++ src/data/simulators/FavoriteAlwaysWins.java
@@ -1,10 +1,23 @@
 package data.simulators;
 
-import records.Match;
 import records.Team;
 
 public class FavoriteAlwaysWins implements Simulator {
-    public Team getWinner(Match match) {
-
+    public Team getWinner(Team team1, Team team2) {
+        if(team1.getElo() > team2.getElo()) {
+            return team1;
+        }
+        else if(team1.getElo() < team2.getElo()) {
+            return team2;
+        }
+        else {
+            int random_num = (int) (Math.random() * 100);
+            if(random_num % 2 == 0) {
+                return team2;
+            }
+            else {
+                return team1;
+            }
+        }
     }
 }

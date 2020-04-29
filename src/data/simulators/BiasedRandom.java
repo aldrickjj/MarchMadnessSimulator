3796c96f9832048b5497c2346b7b23fd35e40d5b
Aldrick Johan <aldrickjohan@gmail.com>
2020-04-28 21:37:55 -0400

    Implemented BiasedRandom

--- src/data/simulators/BiasedRandom.java
+++ src/data/simulators/BiasedRandom.java
@@ -1,4 +1,32 @@
 package data.simulators;
 
-public class BiasedRandom {
+import records.Match;
+import records.Team;
+import java.util.Random;
+
+public class BiasedRandom implements Simulator{
+
+    public BiasedRandom(){}
+    
+    @Override
+    public Team getWinner(Team team1, Team team2) {
+        Team favorite = team1;
+        Team underdog = team1;
+        if(team2.getElo() > team1.getElo())
+            favorite = team2;
+        else
+            underdog = team2;
+
+        int dr = Math.abs(team1.getElo() - team2.getElo());
+        int exp = (-1 * dr) / 400;
+        double bottom = Math.pow(10, exp) + 1;
+        double We = 1 / bottom;
+        Random rand = new Random();
+        double flip = rand.nextDouble();
+
+        if(flip < We)
+            return favorite;
+        else
+            return underdog;
+    }
 }
